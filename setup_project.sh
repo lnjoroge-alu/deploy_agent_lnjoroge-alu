62dfeb91a23653e4e16b1cfb73dc755983745b2a
lnjoroge-alu <l.njoroge@alustudent.com>
2026-02-19 14:01:13 +0300

    Add helper function for exiting program

--- setup_project.sh
+++ setup_project.sh
@@ -1,4 +1,8 @@
 #!/bin/env bash
 
+# Helper functions for error handling 
+die()  { echo "Error: $*" >&2; exit 1; }
+
+
 read -r -p "Enter project identifier (used in attendance_tracker_{input}): " INPUT 
-[[ -n "${INPUT// }" ]]
+[[ -n "${INPUT// }" ]] 

54753df583d2cfd19861344065021030f825c487
lnjoroge-alu <l.njoroge@alustudent.com>
2026-02-19 14:02:41 +0300

    Update read input error handling

--- setup_project.sh
+++ setup_project.sh
@@ -5,4 +5,4 @@ die()  { echo "Error: $*" >&2; exit 1; }
 
 
 read -r -p "Enter project identifier (used in attendance_tracker_{input}): " INPUT 
-[[ -n "${INPUT// }" ]] 
+[[ -n "${INPUT// }" ]] || die "Input cannot be empty."
